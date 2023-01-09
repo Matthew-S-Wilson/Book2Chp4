fbd3bf055be0b31a08ba63fa1dbf275442ae6480
Matthew-S-Wilson <matthew255@gmail.com>
2023-01-09 10:43:25 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const add = (firstNumber, secondNumber) => {
+    sum = firstNumber + secondNumber
+    return sum
+}
+
+const subtract =  (firstNumber, secondNumber) => {
+    sum = firstNumber - secondNumber
+    return sum
+}
+
+const divide =  (firstNumber, secondNumber) => {
+    sum = firstNumber / secondNumber
+    return sum
+}
+
+const multiply =  (firstNumber, secondNumber) => {
+    sum = firstNumber * secondNumber
+    return sum
+}
+
+const square =  (firstNumber) => {
+    sum = firstNumber * firstNumber
+    return sum
+}
+
+let result =  multiply(.25, 4)
+result = multiply(.75,2)
+result = add(1,1.5)
+console.log(result)
